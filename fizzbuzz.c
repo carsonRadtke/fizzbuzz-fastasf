b9916f4700170b3349ab66c165664d01b20fd636
Carson Radtke <nosrac925@gmail.com>
2023-10-06 10:55:21 -0500

    [ 243 MiB/s ] check for i%15 instead of 3 and 5 + refactor

--- fizzbuzz.c
+++ fizzbuzz.c
@@ -1,15 +1,24 @@
 #include <stdio.h>
 
+typedef unsigned int i32;
+
+inline void fizzbuzz(void) { printf("%s\n", "FizzBuzz"); }
+inline void fizz(void) { printf("%s\n", "Fizz"); }
+inline void buzz(void) { printf("%s\n", "Buzz"); }
+inline void number(i32 num) { printf("%d\n", num); }
+
+void fizzbuzz_impl(i32 N) {
+  i32 i = 0;
+  for (; i < N; i++)
+  {
+    if (i % 15 == 0) fizzbuzz();
+    else if (i % 3 == 0) fizz();
+    else if (i % 5 == 0) buzz();
+    else number(i);
+  }
+}
+
 int main() {
-    for (int i = 1; i < 1000000000; i++) {
-        if ((i % 3 == 0) && (i % 5 == 0)) {
-            printf("FizzBuzz\n");
-        } else if (i % 3 == 0) {
-            printf("Fizz\n");
-        } else if (i % 5 == 0) {
-            printf("Buzz\n");
-        } else {
-            printf("%d\n", i);
-        }
-    }
+  fizzbuzz_impl(1'000'000'000);
+  return 0;
 }
