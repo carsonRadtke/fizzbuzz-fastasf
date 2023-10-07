99a192a8bec04a6f1d94080cd1daf203f23e3be0
Carson Radtke <nosrac925@gmail.com>
2023-10-07 16:40:43 -0500

    explicitly declare used function + run format

--- fizzbuzz.c
+++ fizzbuzz.c
@@ -1,15 +1,16 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <string.h>
-
 #define LIMIT 1'000'000'000
 #define BUFFER_SIZE 64 * 1024
 #define STDOUT_FD 1
 #define U32_MAX_STRLEN 11
 
 typedef unsigned int uint32;
+typedef long ssize_t;
+typedef unsigned long size_t;
 
-static const char fizzbuzz_str[] = {'F', 'i', 'z', 'z','B',
+extern ssize_t write(int, const void *, size_t);
+extern void *memcpy(void *, const void *, unsigned long);
+
+static const char fizzbuzz_str[] = {'F', 'i', 'z', 'z', 'B',
                                     'u', 'z', 'z', '\n'};
 static const char fizz_str[] = {'F', 'i', 'z', 'z', '\n'};
 static const char buzz_str[] = {'B', 'u', 'z', 'z', '\n'};
@@ -37,11 +38,10 @@ inline void number(uint32 num) {
   char numbuf[U32_MAX_STRLEN];
   char *numbuf_end = numbuf + sizeof(numbuf);
   char *numbuf_start = numbuf_end;
-  
+
   uint32 n = num;
   *(--numbuf_start) = '\n';
-  do
-  {
+  do {
     *(--numbuf_start) = '0' + (n % 10);
     n /= 10;
   } while (n > 0);
@@ -51,8 +51,7 @@ inline void number(uint32 num) {
 
 void fizzbuzz_impl(uint32 N) {
   uint32 i = 0;
-  for (; i + 15 < N; i += 15)
-  {
+  for (; i + 15 < N; i += 15) {
     fizzbuzz();
     number(i + 1);
     number(i + 2);
